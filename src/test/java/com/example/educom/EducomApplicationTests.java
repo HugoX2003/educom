9c5fe5bc41997cde487e57ee6f2ff17d7921080b
HugoX <hugoms2003xd@gmail.com>
2024-04-11 19:00:48 -0500

    first commit

---
+++ src/test/java/com/example/educom/EducomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
